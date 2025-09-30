625305f088ad9bc584ee2a0cf259439ab18042a7
Alice Campos <alicecarolinacc271@gmail.com>
2025-09-30 12:10:02 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -15,4 +15,28 @@ console.log(arrayVazio);
 
 //Tamanho do array -> length
 let dados = [10, 50, 30]; //tamanho 3 -> legth 3
+
 console.log(dados.length);
+
+contadorDeChances = -1;
+chancesRestantesElement.textContent = MAX_CHANCES; //Mostra o total de chances
+
+// Limpar as mensagens anteriores 
+menssagemElement.textContent = '';
+palpitesAnterioresElement.textContent = '';
+listaDePalpites = []
+
+
+menssagemElement.style.color = '#f0f0f0'; // Cor padrão do texto
+dicaElement.style.color = '#ffeb3b'; // Cor padrão da ética
+
+// Reativar o campo de palpite e o botão de enviar
+campoPalpite.disabled = false;
+botãoEnviarPalpite.disabled = false;
+campoPalpite.value = ''; // Limpa o campo de entrada
+campoPalpite.focus(); // Coloca o cursor para o usuário digitar
+
+// Esconder o botão de reiniciar
+botaoIniciar.classList.add('hidden');
+
+//2. Função para verificar o palpite do jogador
